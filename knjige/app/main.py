e7e758392c6c43bbef24f756b4bdd11a8d0fb853
sladjanapekovic <sladjana.pekovic@student.um.si>
2026-03-16 17:10:03 +0100

    Update main.py

--- knjige/app/main.py
+++ knjige/app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from .database import Base, engine
-from . import models
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from .database import Base, engine, SessionLocal
+from . import models, schemas
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,11 +13,33 @@ app = FastAPI(
 )
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 def root():
     return {"message": "Knjige microservice is running"}
 
 
-@app.get("/books")
-def get_books():
-    return []
+@app.get("/books", response_model=list[schemas.BookResponse])
+def get_books(db: Session = Depends(get_db)):
+    return db.query(models.Book).all()
+
+
+@app.post("/books", response_model=schemas.BookResponse)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    db_book = models.Book(
+        title=book.title,
+        author=book.author,
+        genre=book.genre,
+        available_copies=book.available_copies
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book

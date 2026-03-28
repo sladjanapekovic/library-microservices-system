ec0ca7907dc20c496dbef8555be756b441a0aa7b
sladjanapekovic <sladjana.pekovic@student.um.si>
2026-03-28 19:25:55 +0100

    Update IzposojaApplication.java

--- izposoja/src/main/java/com/library/izposoja/IzposojaApplication.java
+++ izposoja/src/main/java/com/library/izposoja/IzposojaApplication.java
@@ -2,8 +2,10 @@ package com.library.izposoja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class IzposojaApplication {
 
     public static void main(String[] args) {

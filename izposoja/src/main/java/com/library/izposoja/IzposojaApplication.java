9f038ee227aa061610a33af1baaf98dfba2fc8d8
sladjanapekovic <sladjana.pekovic@student.um.si>
2026-03-28 19:01:23 +0100

    Create IzposojaApplication.java

---
+++ izposoja/src/main/java/com/library/izposoja/IzposojaApplication.java
@@ -0,0 +1,12 @@
+package com.library.izposoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzposojaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IzposojaApplication.class, args);
+    }
+}

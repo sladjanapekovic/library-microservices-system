854cc58ee20a7b06460959b855420351998d3c26
sladjanapekovic <sladjana.pekovic@student.um.si>
2026-03-28 17:07:50 +0100

    Create db.js

---
+++ uporabniki/src/db.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: "postgres",
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+  database: "uporabniki_db",
+});
+
+async function initializeDatabase() {
+  const query = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(100) NOT NULL,
+      email VARCHAR(150) NOT NULL
+    );
+  `;
+
+  await pool.query(query);
+  console.log("PostgreSQL database initialized.");
+}
+
+module.exports = {
+  pool,
+  initializeDatabase,
+};

b4367bd788e5dd8278c1c6ea5884cf7dbbf523e8
sladjanapekovic <sladjana.pekovic@student.um.si>
2026-05-25 17:10:29 +0200

    Update database.py

--- knjige/app/database.py
+++ knjige/app/database.py
@@ -1,10 +1,18 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./books.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./books.db")
+
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(

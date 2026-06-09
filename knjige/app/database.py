4383de3391ca980aa65f7573463caa55cc71c1fe
sladjanapekovic <sladjana.pekovic@student.um.si>
2026-06-09 17:42:28 +0200

    Update database.py

--- knjige/app/database.py
+++ knjige/app/database.py
@@ -1,7 +1,9 @@
 import os
+import time
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import OperationalError
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./books.db")
 
@@ -22,3 +24,17 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def wait_for_database(retries=10, delay=3):
+    for attempt in range(retries):
+        try:
+            connection = engine.connect()
+            connection.close()
+            print("Database connection successful.")
+            return
+        except OperationalError:
+            print(f"Database not ready yet. Retrying in {delay} seconds...")
+            time.sleep(delay)
+
+    raise Exception("Database connection failed after multiple retries.")
